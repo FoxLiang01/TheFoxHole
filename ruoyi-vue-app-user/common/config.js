f6bd172c499a2a7c9ca009ad4032d31eafe522b1
Tellsea <tanghh0920>
2022-11-09 17:16:11 +0800

    删除测试模块

--- ruoyi-vue-app-user/common/config.js
+++ ruoyi-vue-app-user/common/config.js
@@ -7,7 +7,7 @@ export default {
     cachePrefix: 'ruoyi-vue-app-user:',
 
     // 请求、响应数据加密
-    aesEncryptEnabled: true,
+    aesEncryptEnabled: false,
     // 请求数据加密秘钥串
     aesEncryptKey: 'b8123298c638fb4c',
 }
